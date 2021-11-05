7a724f0d550200110f73bb90504014440c9af1a1
Samuel Thomas <sgpthomas@gmail.com>
2021-11-05 13:17:52 -0500

    tried to be smarter about detecting redundant things but now it causes emacs to hang

--- ob-acl2.el
+++ ob-acl2.el
@@ -27,11 +27,15 @@
     (let* ((buf-name (format "*acl2-session:%s*" session))
 	   (proc-name (org-babel-acl2--proc-name session)))
       (make-comint-in-buffer proc-name buf-name org-babel-acl2-command)
-      (message "words: %s" (org-babel-acl2--send-string buf-name ":help"))
+      (org-babel-acl2--send-string buf-name "")
       buf-name)))
 
 (defun org-babel-acl2--proc-name (session)
   (format "acl2<%s>" session))
+
+(defun org-babel-acl2--input-ended (s)
+  (or (string-match "ACL2 !>" s)
+      (string-match "Process acl2<.*> finished" s)))
 
 (defun org-babel-acl2--send-string (session body)
   (org-babel-comint-in-buffer session
@@ -43,15 +47,23 @@
 		  comint-output-filter-functions)))
       (insert body)
       (comint-send-input)
-      (while (not (string-match "ACL2 !>" string-buffer))
-	(accept-process-output (get-buffer-process (current-buffer))))
+      (setq-local limit 10)
+      (while (or (not (org-babel-acl2--input-ended string-buffer))
+		 (zerop limit))
+	(accept-process-output (get-buffer-process (current-buffer)) 1)
+	(setq-local limit (1- limit))
+	(message "limit: %s" limit))
       (--> (substring string-buffer 0 (match-beginning 0))
 	   (org-babel-chomp it)
 	   (org-babel-chomp it)
 	   (org-babel-chomp it)))))
 
 (defun org-babel-acl2-execute (session body)
-  (org-babel-acl2--send-string session body))
+  (let* ((output (org-babel-acl2--send-string session body))
+	 (redundant? (string-match-p ":REDUNDANT" output)))
+    ;; only return output if the definition hasn't already been accepted
+    (when (not redundant?)
+      output)))
 
 ;;;###autoload
 (define-derived-mode acl2-mode lisp-mode "ACL2 Mode"
